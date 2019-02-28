f9581ed296e6bf066b8e07d55611277f594d341c
unknown <will.king2@gmail.com>
2019-02-28 10:35:03 -0800

    making and testing survival function w/ ceiling

--- manually building ceilings.R
+++ manually building ceilings.R
@@ -1,3 +1,5 @@
+
+
 ### manual ceiling example, from IPM_simple
 
 s_z_simple <- function(z, m.par_simple){
@@ -21,3 +23,35 @@ s_z_simple <- function(z, m.par_simple){
   # )
 }
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+### for IPM size-touch
+# ceilings are Uz1 = 6 and Ut1 = 0.8, respectively
+
+### survival: s(z)
+
+s_z_st <- function(z, m.par_st){
+  
+  ## linear predictor
+  
+  # below size ceiling
+  linear.p <- m.par_st['surv.int'] + m.par_st['surv.z'] * z
+  # above size ceiling
+  linear.p.sizeceiling <- m.par_st['surv.int'] + m.par_st['surv.z'] * Uz1
+  
+  # impose ceiling and backtransform from logit
+  p <- ifelse(z <= Uz1
+              , 1/(1+exp(-linear.p)) # below ceiling, use regression coeff of z
+              , 1/(1+exp(-linear.p.sizeceiling)) # above ceiling, use coeff of Uz1
+  )
+  
+  ## output
+  return( unname(p) )
+  
+  # ## back transform from logit
+  # p <- 1/(1+exp(-linear.p))
+}
+
+# test
+s_z_st(3, m.par_st)
+s_z_st(6, m.par_st)
+s_z_st(7, m.par_st)
