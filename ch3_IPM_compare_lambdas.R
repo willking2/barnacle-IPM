bf170b988387a41f051ab4e0f42622b53bf5941b
unknown <will.king2@gmail.com>
2019-03-20 16:05:54 -0700

    started draft plot

--- ch3_IPM_compare_lambdas.R
+++ ch3_IPM_compare_lambdas.R
@@ -5,3 +5,118 @@
 library(scales)
 
 lambdas <- read.csv('ch3_IPM_compare_lambdas.csv', header = T)
+lambdas.model <- lambdas[lambdas$type == 'IPM', ]
+
+# ---- adjust data and calculate terms ----
+
+# make continuous variable for easier plotting
+lambdas.model$cont <- as.numeric(rownames(lambdas.model))
+
+# calculate field mean and se with all data included
+lambda.field.all_mean <- mean(lambdas$lambda[lambdas$type == 'field'])
+lambda.field.all_se <- sd(lambdas$lambda[lambdas$type == 'field'])/sqrt(12)
+lambda.field.all_serange <- c(lambda.field.all_mean - lambda.field.all_se # lower
+                              , lambda.field.all_mean + lambda.field.all_se # upper
+)
+
+# calculate field mean and se with lambdas 2.00 or greater excluded (4 points excluded)
+lambda.field.subset_mean <- mean(lambdas$lambda[lambdas$type == 'field' & lambdas$lambda < 2])
+lambda.field.subset_se <- sd(lambdas$lambda[lambdas$type == 'field'& lambdas$lambda < 2])/sqrt(12)
+lambda.field.subset_serange <- c(lambda.field.subset_mean - lambda.field.subset_se # lower
+                              , lambda.field.subset_mean + lambda.field.subset_se # upper
+)
+
+# ---- plot ----
+
+## blank plot
+plot(lambda ~ cont
+     , data = lambdas.model
+     , ylim = c(0.5, 2.5)
+     , xlim = c(min(cont)-2, max(cont))
+     , type = 'n'
+     , axes = F
+     , xlab = ''
+     , ylab = ''
+     )
+
+## add line of zero population growth
+lines(x = c(min(lambdas.model$cont)-3, max(lambdas.model$cont)+1)
+      , y = c(1, 1)
+      , col = 'gray'
+      , lty = 2
+)
+
+# mean and se of all field lambdas
+lines(x = c(min(lambdas.model$cont) - 2, min(lambdas.model$cont) - 2)
+      , y = c(lambda.field.all_serange[1], lambda.field.all_serange[2])
+)
+points(x = min(lambdas.model$cont) - 2
+       , y = lambda.field.all_mean
+       , pch = 21
+       , bg = 'white'
+)
+
+# mean and se of field lambdas < 2
+lines(x = c(min(lambdas.model$cont) - 1, min(lambdas.model$cont) - 1)
+      , y = c(lambda.field.subset_serange[1], lambda.field.subset_serange[2])
+)
+points(x = min(lambdas.model$cont) - 1
+       , y = lambda.field.subset_mean
+       , pch = 21
+       , bg = 'white'
+)
+
+## 95% CIs of model lambdas
+for (i in min(lambdas.model$cont):max(lambdas.model$cont)){
+  lines(x = c(i, i)
+        , y = c(lambdas.model$confint95_lower[lambdas.model$cont == i]
+                , lambdas.model$confint95_upper[lambdas.model$cont == i])
+  )
+}
+
+## model lambdas in black
+points(lambda ~ cont
+       , data = lambdas.model
+       , ylim = c(0.5, 2.5)
+       , xlim = c(min(cont)-2, max(cont))
+       , col = 'black'
+       , pch = 19
+)
+
+
+# axes and axes labels
+axis(1 
+     , at = seq(from = min(lambdas.model$cont) - 3
+                , to = max(lambdas.model$cont) + 1
+                , by = 1)
+     , pos = 0.5
+     , tck = 0.02
+     , labels = c('', 'field all', 'field subset', as.character(lambdas.model$scenario), '')
+     , las = 2
+)
+axis(2
+     , las = 1
+     , pos = min(lambdas.model$cont) - 3
+     , tck = 0.02
+)
+axis(3
+     , at = seq(from = min(lambdas.model$cont) - 3
+                , to = max(lambdas.model$cont) + 1
+                , by = 1)
+     , pos = 2.5
+     , tck = 0.02
+     , labels = F
+)
+axis(4
+     , las = 1
+     , pos = max(lambdas.model$cont) + 1
+     , tck = 0.02
+     , labels = F
+)
+
+
+mtext('Lambda'
+      , side = 2
+      , line = 3
+      , cex = 1.2
+)
